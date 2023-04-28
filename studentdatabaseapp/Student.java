c31f8a11d1ff8998691f8da71d8df8822c2a4555
elijah obara <elijahobara357@gmail.com>
2023-04-28 15:19:46 +0300

    Student management system prjct

---
+++ studentdatabaseapp/Student.java
@@ -0,0 +1,26 @@
+package studentdatabaseapp;
+
+public class Student
+{
+    private String firstname;
+    private String lastname;
+    private String gradeYear;
+    private int studentsID;
+    private String courses;
+    private int tuitionBalance;
+    private int costOfCourse = 600;
+
+
+    //constructor -> prompt user to enter students name and year
+
+    // gen uique id
+
+    // enroll courses
+
+    //view balance 
+
+    //pay tuition
+
+    // show status
+    
+}
---
+++ studentdatabaseapp/StudentDatabaseApp.java
@@ -0,0 +1,10 @@
+package studentdatabaseapp;
+
+public class StudentDatabaseApp
+{
+    public static void main(String[] args)
+    {
+     // how many users we want to add
+     // create a number of new stdents   
+    }
+}
