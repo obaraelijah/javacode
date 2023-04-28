af62ad18da5fdca5cfe8a54cdcbace5d7d111aa3
elijah obara <elijahobara357@gmail.com>
2023-04-28 13:30:41 +0300

    java email task

---
+++ emailapp/Email.java
@@ -0,0 +1,22 @@
+package emailapp;
+
+public class Email {
+    private String firstName;
+    private String lastName;
+    private String passWord;
+    private String department;
+    private int mailboxCapacity;
+    private String alternateEmail;
+
+    //constructor to receive fast and last name
+
+    // Ask for department
+
+    //generate random password
+
+    // set mailbox capacity
+
+    //set alternate email
+
+    // change password
+}
---
+++ emailapp/EmailApp.java
@@ -0,0 +1,8 @@
+package emailapp;
+
+public class EmailApp {
+    public static void main(String[] args)
+    {
+         //Todo auto-gen method stub
+    }
+}
