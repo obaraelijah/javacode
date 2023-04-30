6f11366eb071d27e5bf6847bebadd3ee217d09a8
elijah obara <elijahobara357@gmail.com>
2023-04-30 12:46:11 +0300

    addToPLayList method

--- SongPlayListApp/Main.java
+++ SongPlayListApp/Main.java
@@ -1,8 +1,26 @@
 package SongPlayListApp;
 
+import java.util.ArrayList;
+
 public class Main {
     
+    private static ArrayList<Album> albums = new ArrayList<>();
     public static void main(String[] args){
         
+        
+        Album album = new Album("Take care","Drake");
+
+        album.addSong("Gods plan", 5.9);
+        album.addSong("Highway to hell",3.5);
+        album.addSong("Kyrptomaniac",5.0);
+        albums.add(album);
+
+        album = new Album("Marshall 2","Eminem");
+
+        album.addSong("Rap god",4.5);
+        album.addSong("Not Afraid",3.5);
+        album.addSong("Lose yourself",4.5);
+        albums.add(album);
+
     }
 }
