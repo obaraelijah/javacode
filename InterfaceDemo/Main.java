3f2d79317511fed196d300d0a1a2979269d7c460
elijah obara <elijahobara357@gmail.com>
2023-05-03 10:49:34 +0300

    iterface prce

--- InterfaceDemo/Main.java
+++ InterfaceDemo/Main.java
@@ -3,6 +3,9 @@ package InterfaceDemo;
 public class Main {
 
     public static void main(String[] args){
-
+        DeskPhone myPhone = new DeskPhone("1234556667");
+        myPhone.powerOn();
+        myPhone.callPhone("1234556667");
+        myPhone.answer();
     }
 }
---
+++ InterfaceDemo/MobilePhone.java
@@ -0,0 +1,48 @@
+package InterfaceDemo;
+
+public class MobilePhone implements Telephone{
+    private String myNumber;
+    private boolean isRinging;
+    private boolean isPowerOn;
+
+    public MobilePhone(String myNumber){
+        this.myNumber = myNumber;
+    }
+
+    @Override
+    public void powerOn() {
+        isPowerOn = true;
+        System.out.println("Phone powered on");
+    }
+
+    @Override
+    public void dial(String phoneNumber) {
+        System.out.println("Now ringing" + phoneNumber + "on deskphone");
+    }
+
+    @Override
+    public void answer() {
+        if(isRinging && isPowerOn){
+           System.out.println("Answering the desk phone");
+           isRinging = false; 
+        }else {
+            System.out.println("Phone is not ringing");
+        }
+    }
+
+    @Override
+    public boolean callPhone(String phoneNUmber) {
+       if(phoneNUmber == myNumber && isPowerOn){
+        isRinging = true;
+        System.out.println("Phone is ringing");
+       }else {
+        isRinging = false;
+       }
+       return isRinging;
+    }
+
+    @Override
+    public boolean isRinging() {
+        return isRinging; 
+    }
+}
