5944eda4c180dc3a24941977ec594675d7db25b8
elijah obara <elijahobara357@gmail.com>
2023-05-03 11:14:41 +0300

    vehicle app

---
+++ vehicleApp/Vehicle.java
@@ -0,0 +1,16 @@
+package vehicleApp;
+
+public class Vehicle {
+    private String name;
+    private int currentSpeed;
+    private int currentDirection;
+
+    public Vehicle(String name) {
+        this.name = name;
+        this.currentSpeed = 0;
+        this.currentDirection = 0;
+    }
+
+
+    
+}
