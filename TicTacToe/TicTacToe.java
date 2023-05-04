1b2a221dd9392b0f93be72f2344c98e5ccecd6ad
elijah obara <elijahobara357@gmail.com>
2023-05-04 14:02:13 +0300

    Hot fix

--- TicTacToe/TicTacToe.java
+++ TicTacToe/TicTacToe.java
@@ -1,11 +1,14 @@
 package TicTacToe;
 
 import java.util.Scanner;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 public class TicTacToe {
     
+    static ArrayList<Integer> playerPositions = new ArrayList<Integer>();
+    static ArrayList<Integer> cpuPositions = new ArrayList<Integer>();
     public static void main(String[] args) {
 
         char[][] gameBoard = {{' ', '|', ' ', '|', ' '},
@@ -89,10 +92,31 @@ public class TicTacToe {
         List topRow = Arrays.asList(1, 2, 3);
         List midRow = Arrays.asList(4, 5, 6);
         List botRow = Arrays.asList(7, 8, 9);
-
         List leftCol = Arrays.asList(1, 4, 7);
         List midCol = Arrays.asList(2, 5, 8);
         List rightCol = Arrays.asList(3, 6, 9);
+        List cross1 = Arrays.asList(1, 5, 9);
+        List cross2 = Arrays.asList(7, 5, 3);
+
+        List<List> winning = new ArrayList<List>();
+        winning.add(topRow);
+        winning.add(midRow);
+        winning.add(botRow);
+        winning.add(leftCol);
+        winning.add(midCol);
+        winning.add(rightCol);
+        winning.add(cross1);
+        winning.add(cross2);
+
+        for(List l : winning) {
+            if(playerPositions.containsAll(l)) {
+                return "Congratulations you won!";
+            } else if(cpuPositions.containsAll(l)) {
+                return "CPU wins! Sorry :(";
+            } else if(playerPositions.size() + cpuPositions.size() == 9) {
+                return "CAT!";
+            }
+        }
 
         return "";
     }
