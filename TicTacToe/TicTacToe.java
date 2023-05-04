180c2c6bce53cb308c52fce8743de4ad5a1cb7c9
elijah obara <elijahobara357@gmail.com>
2023-05-04 10:01:34 +0300

    switch case

--- TicTacToe/TicTacToe.java
+++ TicTacToe/TicTacToe.java
@@ -1,5 +1,7 @@
 package TicTacToe;
 
+import java.util.Scanner;
+
 public class TicTacToe {
     
     public static void main(String[] args) {
@@ -10,6 +12,43 @@ public class TicTacToe {
         {'-', '+', '-', '+', '-'}, 
         {' ', '|', ' ', '|', ' '}};
 
+        printGameBoard(gameBoard);
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter your placement (1-9)");
+        int pos = scan.nextInt();
+
+        System.out.println(pos);
+
+        switch(pos) {
+            case 1:
+                gameBoard[0][0] = 'X';
+                break;
+            case 2:
+                gameBoard[0][2] = 'X';
+                break;
+            case 3:
+                gameBoard[0][4] = 'X';
+                break;
+            case 4:
+                gameBoard[2][0] = 'X';
+                break;
+            case 5:
+                gameBoard[2][2] = 'X';
+                break;
+            case 6:
+                gameBoard[2][4] = 'X';
+                break;
+            case 7:
+                gameBoard[4][0] = 'X';
+                break;
+            case 8:
+                gameBoard[4][2] = 'X';
+                break;
+            case 9:
+                gameBoard[4][4] = 'X';
+                break;
+        }
         printGameBoard(gameBoard);
         
     }
