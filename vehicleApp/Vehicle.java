88d2557e33c74d048e16a629fa4c309133085b00
elijah obara <elijahobara357@gmail.com>
2023-05-04 08:31:00 +0300

    added getters and setters

--- vehicleApp/Vehicle.java
+++ vehicleApp/Vehicle.java
@@ -12,7 +12,38 @@ public class Vehicle {
     }
 
     public void steer(int direction){
-        
+        this.currentDirection += direction;
+        System.out.println("Steer method called: Steering at " + currentDirection + "degrees");
+    }
+
+    public void move(int speed, int direction) {
+        currentSpeed = speed;
+        currentDirection = direction;
+        System.out.println("Move method called: Moving at " + currentSpeed + "in direction " + currentDirection);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCurrentSpeed() {
+        return currentSpeed;
+    }
+
+    public void setCurrentSpeed(int currentSpeed) {
+        this.currentSpeed = currentSpeed;
+    }
+
+    public int getCurrentDirection() {
+        return currentDirection;
+    }
+
+    public void setCurrentDirection(int currentDirection) {
+        this.currentDirection = currentDirection;
     }
         
 }
