ab8c62b24e6a3ae3c4dc32d2625b765e785d3cf1
elijah obara <elijahobara357@gmail.com>
2023-05-04 09:34:35 +0300

    TicTacToe game

---
+++ TicTacToe/TicTacToe.java
@@ -0,0 +1,20 @@
+package TicTacToe;
+
+public class TicTacToe {
+    
+    public static void main(String[] args) {
+
+        char[][] gameBoard = {{' ', '|', ' ', '|', ' '},
+        {'-', '+', '-', '+', '-'} , 
+        {' ', '|', ' ', '|', ' '}, 
+        {'-', '+', '-', '+', '-'}, 
+        {' ', '|', ' ', '|', ' '}};
+
+        for(char[] row : gameBoard ) {
+            for(char c : row) {
+                System.out.print(c);
+            }
+            System.out.println();
+        }
+    }
+}
