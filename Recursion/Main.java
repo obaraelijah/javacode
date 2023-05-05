adbcdce3dc14c89941b5870ec37317cc3ab21c38
elijah obara <elijahobara357@gmail.com>
2023-05-05 10:24:05 +0300

    'fibo

--- Recursion/Main.java
+++ Recursion/Main.java
@@ -87,4 +87,20 @@ public class Main {
             return num % 10 + sum(num / 10);
         }
     }
+
+    public static int count5(int num) {
+        if (num <= 9) {
+            if (num == 5) {
+                return 1;
+            } else {
+                return 0;
+            }
+        } else {
+            if (num % 10 == 5) {
+                return 1 + count5(num / 10);
+            } else {
+                return 0 + count5(num / 10);
+            }
+        }
+    }
 }
